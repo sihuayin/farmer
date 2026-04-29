0f870ffbf7b645b65f6a7b800d84a4b9882b6b72
amber <amber.si@Lbk.one>
2026-04-29 15:23:09 +0800

    chore: 移除侧边导航栏和顶栏
    删除 SideNavBar、TopAppBar，MainLayout 仅包裹 main 区域。

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,12 +1,7 @@
-import SideNavBar from './SideNavBar'
-import TopAppBar from './TopAppBar'
-
 export default function MainLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <SideNavBar />
-      <TopAppBar />
-      <main className="md:ml-64 flex-grow relative">
+    <div className="min-h-screen">
+      <main className="flex-grow relative">
         {children}
       </main>
     </div>
