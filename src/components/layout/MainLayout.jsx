cb009d44a6256807a78b4bb2a89b40e71118d87f
amber <amber.si@Lbk.one>
2026-04-29 15:24:43 +0800

    restore: 恢复侧边导航栏和顶栏

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,7 +1,12 @@
+import SideNavBar from './SideNavBar'
+import TopAppBar from './TopAppBar'
+
 export default function MainLayout({ children }) {
   return (
-    <div className="min-h-screen">
-      <main className="flex-grow relative">
+    <div className="flex flex-col min-h-screen">
+      <SideNavBar />
+      <TopAppBar />
+      <main className="md:ml-64 flex-grow relative">
         {children}
       </main>
     </div>
